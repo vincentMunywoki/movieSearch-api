d4ee05d3946c87631df883806b3d3550af2331ad
vincent munywoki <vincentmunywoki12@gmail.com>
2025-02-11 15:24:45 +0300

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,14 @@ import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com/?apikey=7b30dd88'
 
+const movie = {
+    "Title": "Batman Begins",
+    "Year": "2005",
+    "imdbID": "tt0372784",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BODIyMDdhNTgtNDlmOC00MjUxLWE2NDItODA5MTdkNzY3ZTdhXkEyXkFqcGc@._V1_SX300.jpg"
+}
+
 function App() {
 
   const searchMovie = async (title)=> {
@@ -31,6 +39,19 @@ function App() {
           onChange={() => {}} />
 
           <img src={SearchIcon} alt='search' onClick= {() => { }} />
+        </div>
+
+        <div className="movie">
+          <div>
+            <p>{movie.Year}</p>
+          </div>
+          <div>
+            <img src={movie.Poster} alt={movie.Title} />
+          </div>
+          <div>
+            <span>{movie.Type}</span>
+            <h3>{movie.Title}</h3>
+          </div>
         </div>
       </div>
     </>
