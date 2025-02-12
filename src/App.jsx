8599f9c75fd149f263310a8a82c02f2b7b0974da
vincent munywoki <vincentmunywoki12@gmail.com>
2025-02-12 11:05:07 +0300

    movies-file

--- src/App.jsx
+++ src/App.jsx
@@ -53,7 +53,7 @@ function App() {
             ))}
           </div>
         ) : (
-          // This message is Displayed when no movies are found
+          // This message is Displayed when no movies are found.
           <div className="empty">
             <h2>No Movies Found!</h2>
           </div>
